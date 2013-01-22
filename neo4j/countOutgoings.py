aaf1b796e75d24bc1eab043526e9c15088782a71
Josef M <lotus_mud@gmx.de>
2013-01-22 18:37:42 +0100

    fix neo4j

---
+++ neo4j/countOutgoings.py
@@ -0,0 +1,26 @@
+from neo4j import GraphDatabase
+db = GraphDatabase("/mnt/neo4j_full")
+
+def writeIncomings(arr):
+	arr.sort(key=lambda v:v[2], reverse=True)
+	with open('/home/ubuntu/outgoings', 'w') as f:
+        	for e in arr:
+                	f.write(e[0] + ", " + str(e[1])+", "+str(e[2])+"\n")
+
+
+i = 0
+for anode in db.nodes:
+	i += 1
+	arr.append((anode.get('url',"unknown"), anode.get('title',"uknown"), len(anode.LINKS_TO.outgoing)))
+	if i % 500 == 0:
+		print i
+	if i % 1000 == 0:
+		writeIncomings(arr)
+	
+
+arr.sort(key=lambda v:v[1], reverse=True)
+with open('/home/ubuntu/incomings', 'w') as f:
+	for e in arr:
+		f.write(e[0] + ", " + str(e[1])+"\n")
+
+db.shutdown()
